ebac27973373a1656e0aa692753a2b9cf9fcd243
kkinter <ifol1129@gmail.com>
2023-03-27 19:02:32 +0900

    Feat | inf view

--- src/ratings/views.py
+++ src/ratings/views.py
@@ -10,6 +10,11 @@ def rate_movie_view(request):
         return HttpResponse("Not Allowed", status=400)
     object_id = request.POST.get('object_id')
     rating_value = request.POST.get('rating_value')
+    if object_id is None or rating_value is None:
+        response = HttpResponse("Skipping", status=200)
+        response['HX-Trigger'] = 'did-skip-movie'
+        return response
+
     user = request.user
     message = "You must <a href='/accounts/login'>login</a> to rate this."
     if user.is_authenticated:
@@ -19,5 +24,7 @@ def rate_movie_view(request):
                                            value=rating_value, user=user)
         if rating_obj.content_object is not None:
             message = "<div class='bg-success text-light px-3 py-1 rounded'>Success.</div>"
-    
+            response = HttpResponse("Skipping", status=200)
+            response['HX-Trigger-After-Settle'] = 'did-rate-movie'
+            return response
     return HttpResponse(message, status=200)
